ed0b2a38e37ad23b5112c6a205ca38eb6e68869d
Junior <junior@MacBook-Pro-de-Jose.local>
2021-08-16 09:17:09 -0300

    16/08/2021 - Correção Startup

--- Api.Domain/Interfaces/Repositories/IUserRepository.cs
+++ Api.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -5,6 +5,12 @@ namespace Api.Domain.Interfaces.Repositories
 {
     public interface IUserRepository : IRepository<UserEntity>
     {
-        Task<UserEntity> FindByLogin(string email, string password);
+        Task<UserEntity> FindByLogin(string email, string password); // Seleciona email e pass para poder logar 
+
+        Task<UserEntity> GetByEmail(string email); //seleciona um email específico
+
+        Task<IEnumerable<UserEntity>> SearchByEmail(string email); //seleciona todos os emails que tem em trecho da procura
+
+        Task<IEnumerable<UserEntity>> SearchByName(string name);
     }
 }
